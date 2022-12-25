d9e49ea34fcaa23889ba9f36bdec96e2d5d50cff
Damir <damir.ps04@gmail.com>
2022-12-26 03:27:08 +0600

    first commit

---
+++ go.mod
@@ -0,0 +1,20 @@
+module github.com/shynggys9219/greenlight
+
+go 1.18
+
+require (
+	github.com/julienschmidt/httprouter v1.3.0
+	github.com/pressly/goose v2.7.0+incompatible
+)
+
+require (
+	github.com/go-playground/locales v0.14.0 // indirect
+	github.com/go-playground/universal-translator v0.18.0 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
+	gopkg.in/go-playground/validator.v9 v9.31.0 // indirect
+)
+
+require (
+	github.com/lib/pq v1.10.2
+	github.com/pkg/errors v0.9.1 // indirect
+)
---
+++ internal/data/movies.go
@@ -0,0 +1,122 @@
+package data
+
+import (
+	"database/sql"
+	"errors"
+	"time"
+
+	"github.com/lib/pq"
+)
+
+// By default, the keys in the JSON object are equal to the field names in the struct ( ID,
+// CreatedAt, Title and so on).
+type Movie struct {
+	ID        int64     `json:"id"`                       // Unique integer ID for the movie
+	CreatedAt time.Time `json:"-"`                        // Timestamp for when the movie is added to our database, "-" directive, hidden in response
+	Title     string    `json:"title"`                    // Movie title
+	Year      int32     `json:"year,omitempty"`           // Movie release year, "omitempty" - hide from response if empty
+	Runtime   int32     `json:"runtime,omitempty,string"` // Movie runtime (in minutes), "string" - convert int to string
+	Genres    []string  `json:"genres,omitempty"`         // Slice of genres for the movie (romance, comedy, etc.)
+	Version   int32     `json:"version"`                  // The version number starts at 1 and will be incremented each
+	// time the movie information is updated
+}
+
+// Define a MovieModel struct type which wraps a sql.DB connection pool.
+type MovieModel struct {
+	DB *sql.DB
+}
+
+// method for inserting a new record in the movies table.
+func (m MovieModel) Insert(movie *Movie) error {
+	query := `
+		INSERT INTO movies(title, year, runtime, genres)
+		VALUES ($1, $2, $3, $4)
+		RETURNING id, created_at, version`
+
+	return m.DB.QueryRow(query, &movie.Title, &movie.Year, &movie.Runtime, pq.Array(&movie.Genres)).Scan(&movie.ID, &movie.CreatedAt, &movie.Version)
+}
+
+func (m MovieModel) Get(id int64) (*Movie, error) {
+	if id < 1 {
+		return nil, ErrRecordNotFound
+	}
+	query := `
+SELECT id, created_at, title, year, runtime, genres, version FROM movies
+WHERE id = $1`
+	var movie Movie
+	err := m.DB.QueryRow(query, id).Scan(&movie.ID,
+		&movie.CreatedAt, &movie.Title, &movie.Year, &movie.Runtime, pq.Array(&movie.Genres), &movie.Version,
+	)
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return nil, ErrRecordNotFound
+		default:
+			return nil, err
+		}
+	}
+	return &movie, nil
+}
+func (m MovieModel) GetByTitle(title string) (*Movie, error) {
+	if title < "null" {
+		return nil, ErrRecordNotFound
+	}
+	query := `
+SELECT id, created_at, title, year, runtime, genres, version FROM movies
+WHERE title = $1`
+	var movie Movie
+	err := m.DB.QueryRow(query, title).Scan(&movie.ID,
+		&movie.CreatedAt, &movie.Title, &movie.Year, &movie.Runtime, pq.Array(&movie.Genres), &movie.Version,
+	)
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return nil, ErrRecordNotFound
+		default:
+			return nil, err
+		}
+	}
+	return &movie, nil
+}
+func (m MovieModel) Update(movie *Movie) error {
+	// Declare the SQL query for updating the record and returning the new version     // number.
+	query := `
+	UPDATE movies
+	SET title = $1, year = $2, runtime = $3, genres = $4, version = version + 1
+	WHERE id = $5
+	RETURNING version`
+	// Create an args slice containing the values for the placeholder parameters.
+	args := []any{
+		movie.Title,
+		movie.Year,
+		movie.Runtime,
+		pq.Array(movie.Genres),
+		movie.ID,
+	}
+	// Use the QueryRow() method to execute the query, passing in the args slice as a     // variadic parameter and scanning the new version value into the movie struct.
+	return m.DB.QueryRow(query, args...).Scan(&movie.Version)
+}
+
+// method for deleting a specific record from the movies table.
+func (m MovieModel) Delete(id int64) error {
+	// Return an ErrRecordNotFound error if the movie ID is less than 1.
+	if id < 1 {
+		return ErrRecordNotFound
+	}
+	query := `DELETE FROM movies WHERE id = $1`
+	result, err := m.DB.Exec(query, id)
+	if err != nil {
+		return err
+	}
+	// Call the RowsAffected() method on the sql.Result object to get the number of rows     // affected by the query.
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+	// If no rows were affected, we know that the movies table didn't contain a record
+	// with the provided ID at the moment we tried to delete it. In that case we     // return an ErrRecordNotFound error.
+	if rowsAffected == 0 {
+		return ErrRecordNotFound
+	}
+	return nil
+}
