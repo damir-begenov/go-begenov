f6288e3242760e3856c2c15bb1c3d5be04ec7845
Damir <damir.ps04@gmail.com>
2023-01-09 13:43:41 +0600

    kjl

--- migrations/000002_add_movies_check_constraints.down.sql
+++ migrations/000002_add_movies_check_constraints.down.sql
@@ -1,5 +1,4 @@
 ALTER TABLE movies DROP CONSTRAINT IF EXISTS movies_runtime_check;
 ALTER TABLE movies DROP CONSTRAINT IF EXISTS movies_year_check;
 ALTER TABLE movies DROP CONSTRAINT IF EXISTS genres_length_check;
-migrate -source file://Users/damirbegenov/Downloads/greenlight-2/migrations -database "postgres://localhost:5432/postgres:0000/postgres?sslmode=disable" up
 
